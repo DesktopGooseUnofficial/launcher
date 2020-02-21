f509add16845517eb54fadc9a63d835a2e1a3718
Lil Sizzurp <45602120+LilSizzurp@users.noreply.github.com>
2020-02-22 00:19:30 +0100

    h

--- ResourceHubLauncher/ModButton.cs
+++ ResourceHubLauncher/ModButton.cs
@@ -10,14 +10,13 @@ namespace ResourceHubLauncher {
         Disabled,
         Available
     }
-    class ModButton 
-    {
+    class ModButton {
         public ModButton(string modName_, string modSafetyName, int modSafetyLevel, ModButtonStates modState_) {
             button = new MetroButton();
             modName = new MetroLabel();
             modSafety = new MetroLabel();
             modState = new MetroLabel();
-            button.Size =new Size(177, 88);
+            button.Size = new Size(177, 88);
             button.Text = "";
             button.Theme = MetroFramework.MetroThemeStyle.Dark;
             modName.Theme = MetroFramework.MetroThemeStyle.Dark;
@@ -46,45 +45,44 @@ namespace ResourceHubLauncher {
                     break;
             }
 
-            setLocation(new Point(0,0));
+            setLocation(new Point(0, 0));
         }
 
         public void setLocation(Point newLocation) {
             button.Location = newLocation;
-            modName.Location = new Point(newLocation.X+10, newLocation.Y + 9);
+            modName.Location = new Point(newLocation.X + 10, newLocation.Y + 9);
             modState.Location = new Point(newLocation.X + 10, newLocation.Y + 59);
-            modSafety.Location = new Point(newLocation.X + button.Size.Width- modSafety.Size.Width-10, newLocation.Y + 59);
-            
-            
+            modSafety.Location = new Point(newLocation.X + button.Size.Width - modSafety.Size.Width - 10, newLocation.Y + 59);
+
+
         }
 
-        
+
 
         private MetroButton button;
-        private MetroLabel modName ;
-        private MetroLabel modSafety ;
-        private MetroLabel modState ;
+        private MetroLabel modName;
+        private MetroLabel modSafety;
+        private MetroLabel modState;
     }
 
-    class ModButtonList
-    {
+    class ModButtonList {
         public List<ModButton> list;
-        Point latestAddedPos=new Point(0, -88);
-        Point Location=new Point(0,0);
+        Point latestAddedPos = new Point(0, -88);
+        Point Location = new Point(0, 0);
         public ModButtonList() {
             list = new List<ModButton>();
         }
 
         public void Add(ModButton mod) {
-            mod.setLocation(new Point( Location.X, Location.Y+ latestAddedPos.Y + 88));
+            mod.setLocation(new Point(Location.X, Location.Y + latestAddedPos.Y + 88));
             list.Add(mod);
-            
+
         }
 
         public void setLocation(Point newLocation) {
             Location = newLocation;
             int actualButton = 0;
-            foreach(ModButton b in list) {
+            foreach (ModButton b in list) {
                 b.setLocation(new Point(newLocation.X, newLocation.Y + 88 * actualButton));
                 actualButton++;
             }
