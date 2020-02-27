08f2f608d7c8c302a75cabe170d1b1d10c8e41bf
1JQ <46265927+1JQ@users.noreply.github.com>
2020-02-27 17:14:04 +0100

    updtr

--- RHL Updater/RHL Updater/Form1.Designer.cs
+++ RHL Updater/RHL Updater/Form1.Designer.cs
@@ -1,14 +1,12 @@
-﻿namespace RHL_Updater
-{
-    partial class Form1
-    {
+﻿namespace RHL_Updater {
+    partial class Form1 {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing) {
@@ -21,8 +19,8 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
             this.components = new System.ComponentModel.Container();
--- RHL Updater/RHL Updater/Form1.cs
+++ RHL Updater/RHL Updater/Form1.cs
@@ -8,13 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace RHL_Updater
-{
-    public partial class Form1 : Form
-    {
+namespace RHL_Updater {
+    public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
         }
-
     }
 }
