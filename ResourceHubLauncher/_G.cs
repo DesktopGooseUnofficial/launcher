c99a05d2d7a13723a323e13b40ffe10b8fee66a6
1JQ <46265927+1JQ@users.noreply.github.com>
2020-02-29 13:16:12 +0100

    B E T A

--- ResourceHubLauncher/_G.cs
+++ ResourceHubLauncher/_G.cs
@@ -2,5 +2,7 @@
     class _G {
         public static bool dev => (bool)Config.Options["devmd"];
         public static bool update => (bool)Config.Options["autoUpdate"];
+
+        public static bool beta => (bool)Config.Options["beta"];
     }
 }
